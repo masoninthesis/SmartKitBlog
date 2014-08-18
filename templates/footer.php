3d7ae9fc7c0cfc61aaf8d406a559aa64e34a5686
Mason L <mason.lawlor@me.com>
2014-08-18 14:49:50 -0700

    TheCreation

---
+++ templates/footer.php
@@ -0,0 +1,15 @@
+<footer class="content-info" role="contentinfo">
+  
+ <section>
+  <div class="container">
+  <div class="row">
+	  	<div class="col-sm-12">
+		  	<div class="alert alert-footer alert-t4tg">
+		  	<a class="footer-brand" href="<?php echo home_url(); ?>/"><img src="http://masonlawlor.com//smartkit/assets/images/logo.png" alt="logo-nav" width="auto" height="28px" /></a>
+		  	<p class="copyright">&copy; 2014</p>
+		</div>
+  </div>
+ </section>
+</footer>
+
+<?php wp_footer(); ?>
---
+++ templates/header-top-navbar.php
@@ -0,0 +1,57 @@
+<header class="banner navbar navbar-default navbar-static-top" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="http://masonlawlor.com//smartkit/assets/images/logo.png" alt="logo-nav" width="auto" height="48px" /></a>
+    </div>
+
+    <nav class="collapse navbar-collapse" role="navigation">
+    <button type="button" class="pull-right btn btn-default nav-btn">Back on Kickstarter</button>
+      <?php
+        if (has_nav_menu('primary_navigation')) :
+          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
+        endif;
+      ?>
+    </nav>
+  </div>
+</header>
+
+  <section id="above-the-fold">
+  	<video id="videobcg" preload="auto" autoplay="true" loop="loop" muted="muted" volume="0">
+     		<source src="http://masonlawlor.com//smartkit/assets/video/timelapse.mp4" type="video/mp4">
+     		<source src="http://masonlawlor.com//smartkit/assets/video/timelapse.ogv" type="video/ogv">
+          Sorry, your browser does not support HTML5 video.
+	</video>
+    <div class="wrap container" role="document">
+      <div class="content row">
+        <div class="col-sm-12">
+          <h1>No Contracts.</h1>
+          <h3>How's that for a <span class="smart-home">Smart Home?</span></h3>
+          <a href="//fast.wistia.net/embed/iframe/6y2igsba97?popover=true" class="wistia-popover[height=360,playerColor=ebe9e8,width=640]"><img src="https://embed-ssl.wistia.com/deliveries/f1bd9016d491b25c2af006aefcd2023bcf92e9ce.jpg?image_play_button=true&image_play_button_color=ebe9e8e0&image_crop_resized=300x168" alt="" /></a>
+<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/popover-v1.js"></script>
+<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/popover-v1.js"></script>
+        </div>
+          <div class="col-sm-12 jack-cols">
+			  <button type="button" class="btn btn-default hero-btn">Back on Kickstarter</button>
+              <!--<a class="hero-sections" href="#"><img src="http://masonlawlor.com//smartkit/assets/images/security-hero.png" alt="the-athlete" />
+              <h4>Security</h4></a>
+          </div>
+          <div class="col-sm-4 jack-cols">
+              <a class="hero-sections" href="#"><img src="http://masonlawlor.com//smartkit/assets/images/energy-hero.png" alt="the-fit" />
+              <h4>Energy</h4></a>
+          </div>
+          <div class="col-sm-4 jack-cols">
+              <a class="hero-sections" href="#"><img src="http://masonlawlor.com//smartkit/assets/images/automation-hero.png" alt="the-restored" />
+              <h4>Automation</h4></a>-->
+          </div>
+        </div>
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+  </section>
+<br />
+
