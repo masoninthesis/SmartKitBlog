db651d1474fc15a59138db31e9b7576123278cf3
Mason L <mason.lawlor@me.com>
2014-08-19 23:45:27 -0700

    Post H2– not H1

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,7 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <h2 class="entry-title"><?php the_title(); ?></h2>
       <?php get_template_part('templates/entry-meta'); ?>
       <small class="timestamp"><?php the_time('F jS, Y') ?></small>
     </header>
